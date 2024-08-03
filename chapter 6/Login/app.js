64bf0c67e2cb985629079d8d17ee66ba54cee2aa
Rao Danish <147903668+RaoDanishh@users.noreply.github.com>
2024-08-03 10:31:42 +0500

    commit

---
+++ chapter 6/Login/app.js
@@ -0,0 +1,21 @@
+import {
+    auth,
+    signInWithEmailAndPassword
+} from"/utils/app.js"
+
+let email = document.querySelector(".email");
+let button = document.querySelector("button");
+let password = document.querySelector(".password");
+button.addEventListener("click", ()=> {
+  signInWithEmailAndPassword(auth,email.value, password.value)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    window.location.href="/main/index.html";
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+  });
+
+}) 
